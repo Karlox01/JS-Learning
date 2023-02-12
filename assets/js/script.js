9227afb60c7e0414b9a4499ee8cc81badd7a0059
Karlox01 <orlandinikarlo@gmail.com>
2023-02-12 20:36:11 +0000

    add multiplication to game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,15 +12,18 @@ document.addEventListener('DOMContentLoaded', function() {
             } else {
                 let gameType = this.getAttribute('data-type');
                 runGame(gameType);
-
             }
 
         })
+
     }
-
+    
     runGame('addition');
-
 })
+
+
+
+
 /**
  * Docstring are used to desribe the functions , and should be used right before the function is declared.
  * The main game "loop", called when the script is first loaded
@@ -34,6 +37,8 @@ function runGame(gameType) {
 
     if(gameType === 'addition') {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === 'multiply') {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -73,6 +78,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -101,6 +108,10 @@ function incrementWrongAnswer() {
     let oldScore = parseInt(document.getElementById('incorrect').innerText);
     document.getElementById('incorrect').innerText = ++oldScore; 
 
+    if (oldScore == 5) {
+        alert(`You faggghet you lose!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`)
+        document.body.style.background = "red";
+    }
 }
 
 function displayAdditionQuestion(operand1, operand2) {
@@ -114,6 +125,10 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
